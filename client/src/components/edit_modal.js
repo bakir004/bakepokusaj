050d5d7e46b3dc368ec8dc438cf6bfde35f3a729
Zakir Cinjarevic <zakircin@gmail.com>
2020-01-11 10:47:47 +0100

    d

--- client/src/components/edit_modal.js
+++ client/src/components/edit_modal.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Input from "./input.js"
 import InputIcon from "./inputIcon.js"
 import "../css/modals.css"
 
 export default function MaxWidthDialog(props) {
-    const [media, setMedia] = useState(null)
+    let [name, setName] = useState("")
+    let [country, setCountry] = useState("")
+    let [image, setImage] = useState("")
+    let [artist, setArtist] = useState("")
+    let [genres, setGenres] = useState([])
+    let [producers, setProducers] = useState([])
+    let [media, setMedia] = useState(null)
     const [originalMedia, setOriginalMedia] = useState(null)
     const [warning, showWarning] = useState(false)
     const handleClose = () => {
@@ -19,16 +22,82 @@ export default function MaxWidthDialog(props) {
     };
 
     useEffect(() => {
-        setMedia(props.media);
+        console.log("props effect")
         setOriginalMedia(props.media);
+        setGenres(props.media.genres)
+        setName(props.media.name)
+        setCountry(props.media.country)
+        setImage(props.media.image)
+        setArtist(props.media.artist)
+        setProducers(props.media.producers)
     }, [props])
 
-    const handleInputChanges = event => {
-        let editedMedia = media;
-        editedMedia[event.target.name] = event.target.value
+    useEffect(() => {
+        console.log('aa')
+        let media = {
+            name,
+            image,
+            country,
+            artist,
+            genres,
+            producers
+        }
+        setMedia(media)
+        console.log(media)
+    }, [country, name, artist, image, genres, producers])
+
+    const handleInputChanges = (event, i) => {
+        let editedMedia = originalMedia;
+        editedMedia[event.target.name] = event.target.value;
+
+        // setMedia(editedMedia)
+        // eslint-disable-next-line
+        switch (event.target.name) {
+            case "name": {
+                setName(editedMedia.name)
+                break;
+            }
+            case "country": {
+                setCountry(editedMedia.country)
+                break;
+            }
+            case "image": {
+                setImage(editedMedia.image)
+                break;
+            }
+            case "artist": {
+                setArtist(editedMedia.artist)
+                break;
+            }
+            case "genres": {
+                setGenres(editedMedia.genres)
+                break;
+            }
+            case "producers": {
+                setProducers(editedMedia.producers)
+                break;
+            }
+        }
+
         if (!editedMedia[event.target.name])
             showWarning(true);
         else showWarning(false)
+    }
+    const addNewGenreInput = () => {
+        let newGenres = genres;
+        newGenres.push("");
+        setGenres(newGenres)
+
+    }
+    const removeGenreInput = () => {
+
+    }
+    const genreChange = (genre, inputId) => {
+        let tempGenres = genres;
+        tempGenres[inputId] = genre
+        console.log("TCL: genreChange -> tempGenres", tempGenres)
+
+        setGenres(tempGenres)
     }
     return (
         <React.Fragment>
@@ -47,29 +116,22 @@ export default function MaxWidthDialog(props) {
                                 <div className="col-md-6 modal-input-divs">
 
                                     <DialogContent style={{ marginBottom: "10px" }}>
-                                        <Input name="name" value={props.media.name} onChangeFunction={handleInputChanges}></Input>
-                                        <Input name="country" value={props.media.country} onChangeFunction={handleInputChanges}></Input>
+                                        <Input name="name" value={name} onChangeFunction={handleInputChanges}></Input>
+                                        <Input name="country" value={country} onChangeFunction={handleInputChanges}></Input>
 
                                         <div className="invisible-inputs"> {/* moves the inputs from the 2nd column into the 1st at small window sizes */}
-                                            <Input name="artist" value={props.media.artist} onChangeFunction={handleInputChanges}></Input>
-                                            <Input name="image" value={props.media.image} onChangeFunction={handleInputChanges}></Input>
+                                            <Input name="artist" value={artist} onChangeFunction={handleInputChanges}></Input>
+                                            <Input name="image" value={image} onChangeFunction={handleInputChanges}></Input>
                                         </div>
 
-                                        {media ? media.genres ? media.genres.map((item, i) => (
-                                            <Input key={i} name="genre" value={props.media.genres[i]} onChangeFunction={handleInputChanges}></Input>
+                                        {genres ? genres.map((item, i) => (
+                                            i === 0 ?
+                                                <InputIcon value={item} icon="plus" func={addNewGenreInput} id={i} genreChange={genreChange}></InputIcon> : i === genres.length - 1 ?
+                                                    <InputIcon value={item} icon="minus" func={removeGenreInput} id={i} genreChange={genreChange}></InputIcon> :
+                                                    <InputIcon value={item} id={i} genreChange={genreChange}></InputIcon>
                                         ))
 
-                                            : null : null}
-                                        {media ? media.genres ? media.genres.map((item, i) => (
-                                            <Input key={i} name="genre" value={props.media.genres[i]} onChangeFunction={handleInputChanges}></Input>
-                                        ))
-
-                                            : null : null}
-                                        {media ? media.genres ? media.genres.map((item, i) => (
-                                            <Input key={i} name="genre" value={props.media.genres[i]} onChangeFunction={handleInputChanges}></Input>
-                                        ))
-
-                                            : null : null}
+                                            : null}
                                     </DialogContent>
 
                                 </div>
@@ -95,30 +157,32 @@ export default function MaxWidthDialog(props) {
                             </Button>
                         </div>
                         <div className="col-md-4 col-sm-12">
-                            <DialogContent style={{ marginBottom: "10px" }}>
-                                <div className="modal-media">
-                                    <div className="modal-preview">PREVIEW</div>
-                                    <div className="modal-image-div">
-                                        {/* { eslint-disable-next-line */}
-                                        <img alt="Please fill in the image input with a valid link" className="modal-image" src={media ? media.image : null}></img>
+                            <div className="modal-preview">
+                                <DialogContent style={{ marginBottom: "10px" }}>
+                                    <div className="modal-media">
+                                        <div className="modal-preview">PREVIEW</div>
+                                        <div className="modal-image-div">
+                                            {/* eslint-disable-next-line */}
+                                            <img alt="Please fill in the image input with a valid link" className="modal-image" src={media ? media.image : null}></img>
+                                        </div>
+                                        {media ? <div className="modal-name"> {media.name} </div> : null}
+
+                                        {media ? <div className="modal-artist">By: {media.artist}</div> : null}
+
+                                        {media ? <span className="modal-producers">Producers: </span> : null}
+                                        {media ? media.producers ? media.producers.map((item, i) => (
+                                            <span key={i} className="modal-producers">{item}{media.producers[i + 1] ? <span>, </span> : <br />} </span>
+                                        )) : null : null}
+
+                                        {media ? media.genres ? media.genres.map((item, i) => (
+                                            <div key={i} className="modal-genres">{item}</div>
+                                        )) : null : null}
+
+                                        {media ? <div className="modal-country">Country: {media.country}</div> : null}
+
                                     </div>
-                                    {media ? <div className="modal-name"> {media.name} </div> : null}
-
-                                    {media ? <div className="modal-artist">By: {media.artist}</div> : null}
-
-                                    {media ? <span className="modal-producers">Producers: </span> : null}
-                                    {media ? media.producers ? media.producers.map((item, i) => (
-                                        <span key={i} className="modal-producers">{item}{media.producers[i + 1] ? <span>, </span> : <br />} </span>
-                                    )) : null : null}
-
-                                    {media ? media.genres ? media.genres.map((item, i) => (
-                                        <div key={i} className="modal-genres">{item}</div>
-                                    )) : null : null}
-
-                                    {media ? <div className="modal-country">Country: {media.country}</div> : null}
-
-                                </div>
-                            </DialogContent>
+                                </DialogContent>
+                            </div>
                         </div>
                     </div>
                 </DialogContent>
--- client/src/components/inputIcon.js
+++ client/src/components/inputIcon.js
@@ -1,9 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
@@ -22,19 +18,25 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function InputWithIcon(props) {
+    const [temp, setTemp] = useState('')
     const classes = useStyles();
+    useEffect(() => {
+        setTemp(props.value)
+    }, [])
 
+    function handleChange(event) {
+        setTemp(event.target.value)
+        props.genreChange(temp, props.id)
+    }
     return (
-        <div className={classes.margin}>
-            <Grid container spacing={1} alignItems="flex-end" >
-                <Grid item style={{ width: "256px" }}>
-                    <TextField className={classes.width} id="input-with-icon-grid" label="With a grid" />
-                </Grid>
-                <Grid item style={{ display: "inline" }}>
-                    {props.icon === "plus" ? <AddCircleIcon onClick={props.func} style={{ cursor: "pointer" }} /> : null}
-                    {props.icon === "minus" ? <RemoveCircleIcon onClick={props.func} style={{ cursor: "pointer" }} /> : null}
-                </Grid>
+        <Grid container spacing={1} alignItems="flex-end" >
+            <Grid item style={{ width: "256px" }}>
+                <TextField className={classes.width} value={temp} id="input-with-icon-grid" label="Enter a genre" onChange={(event) => handleChange(event)} />
             </Grid>
-        </div>
+            <Grid item style={{ display: "inline" }}>
+                {props.icon === "plus" ? <AddCircleIcon onClick={props.func} style={{ cursor: "pointer" }} /> : null}
+                {props.icon === "minus" ? <RemoveCircleIcon onClick={props.func} style={{ cursor: "pointer" }} /> : null}
+            </Grid>
+        </Grid>
     );
 }
